1c6b0cae0e32d3b70b5a667d0ec2de962af494e0
Jake Bowkett <jake.bowkett01@gmail.com>
2021-11-28 12:29:56 +1100

    Add production flag to build script.

--- build.sh
+++ build.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-(cd ./cmd && go build -o ./storybot.exe)
+
+# Pass the -p flag to build for the server.
+if [ $# -ge 1 ] && [ "$1" == "-p" ]; then
+    mkdir -p ./build/cmd
+    (cd ./cmd && env GOOS=linux go build -o ../build/cmd/storybot.exe)
+    cp -R ./db ./build/
+    cp ./start.sh ./build/
+    cp ./env.toml ./build/
+    cp ./command*.toml ./build/
+else
+    (cd ./cmd && go build -o ./storybot.exe)
+fi
