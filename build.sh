7a8c330fd85d788a640be27d4af0579ab21b4226
Jake Bowkett <jake.bowkett01@gmail.com>
2021-11-30 07:50:30 +1100

    Rename production output directory from "build" to "dist" to avoid tab complete conflict in shell.

--- build.sh
+++ build.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-# Pass the -p flag to build for the server.
+# Pass the -p flag to build for production.
 if [ $# -ge 1 ] && [ "$1" == "-p" ]; then
-    mkdir -p ./build/cmd
-    (cd ./cmd && env GOOS=linux go build -o ../build/cmd/storybot.exe)
-    cp -R ./db ./build/
-    cp ./start.sh ./build/
-    cp ./env.toml ./build/
-    cp ./command*.toml ./build/
-else
-    (cd ./cmd && go build -o ./storybot.exe)
+    mkdir -p ./dist/cmd
+    (cd ./cmd && env GOOS=linux go build -o ../dist/cmd/storybot)
+    cp -R ./db ./dist/
+    cp ./prod_start.sh ./dist/start.sh
+    cp ./end.sh ./dist/
+    cp ./env.toml ./dist/
+    cp ./command.toml ./dist/
+    cp ./command_data.toml ./dist/
+    echo "Production build complete."
+    exit 0
 fi
+
+# Local development build.
+(cd ./cmd && go build -o ./storybot.exe)
+echo "Development build complete."
